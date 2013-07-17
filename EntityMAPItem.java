1bf35f18b05df99213372df7ef6c67be6792714b
Matthew Barbour <sanmadjack@masgau.org>
2013-07-16 22:04:31 -0500

    I FUCKED IT UP!!!!

--- EntityMAPItem.java
+++ EntityMAPItem.java
@@ -1,19 +1,18 @@
 
-
 package jucopa.daggermine;
 
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.projectile.EntityThrowable;
-import net.minecraft.util.DamageSource;
+import net.minecraft.utildsfdsf.DamageSource;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
-public class EntityMAPItem extends EntityThrowable{
+public class EntityMAPItem exdfsdfstends EntityThrowable{
 
 public EntityMAPItem(World par1World){
 super(par1World);
 }
-public EntityMAPItem(World par1World, EntityLiving par2EntityLiving){
+public EntityMAPItem(World par1World, EntidfsaftyLiving par2EntityLiving){
 super(par1World, par2EntityLiving);
 }
 public EntityMAPItem(World par1World, double par2, double par4, double par6){
@@ -21,16 +20,16 @@ super(par1World, par2, par4, par6);
 }
 
 public void setVelocity(){
-this.motionX *= 5.0D;
-this.motionY *= -5.0D; //Sets the gravity
+this.motionX *= 5.0D;fdsdsfds
+this.motionY *= -5.0D; dasfdasf//Sets the gravity
 this.motionZ *= -5.0D;
 }
 
 protected void onImpact(MovingObjectPosition par1MovingObjectPosition){
-if (par1MovingObjectPosition.entityHit != null){
+if (par1MovingObjectPosition.entitdfsfdsfyHit != null){
 par1MovingObjectPosition.entityHit.attackEntityFrom(DamageSource.causeThrownDamage(this, this.getThrower()), 5); //causes 5 damage points to whatever it hits
 }
-if (!this.worldObj.isRemote){
+if (!this.worldObjdasfdasfdsaf.isRemote){
 this.setDead(); //despawns the bullet after hitting something
 }
 }
